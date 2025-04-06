1f89869fb8e66be922eafe8536d1c2d0805ee751
TEMMINAMIT <temminamit@gmail.com>
2025-04-06 17:09:04 +0700

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public float speed = 1.2f;
+
+    public int health = 5;
+
+    public int level = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        // oyu
-        print("Hello,Danya!");
+        health += level; // Значение жизней соответствует значению уровня.
+
+        print(health); // Вывод значение жизней в консоль. 
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position;
+
+        newPosition.z +=
+
+        speed * Time.deltaTime;
+
+        transform.position = newPosition;
+
     }
 }
